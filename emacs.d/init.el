28c02b2e9a5a431583ff3a967c95aac91e7f1939
Petre Pircalabu <ppircalabu@bitdefender.com>
2018-07-31 14:43:47 +0300

    emacs: setup backup directory
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -57,3 +57,16 @@
 
 (require 'airline-themes)
 (load-theme 'airline-dark)
+
+;; Backups
+(defvar emacs-autosave-directory
+  (concat user-emacs-directory "autosaves/")
+  "This variable dictates where to put auto saves. It is set to a
+  directory called autosaves located wherever your .emacs.d/ is
+  located.")
+
+;; Sets all files to be backed up and auto saved in a single directory.
+(setq backup-directory-alist
+      `((".*" . ,emacs-autosave-directory))
+      auto-save-file-name-transforms
+      `((".*" ,emacs-autosave-directory t)))
